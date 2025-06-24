1ee6deafcafed80f03928c7301d58c647a048db0
Gjh99 <1737035429@qq.com>
2025-06-24 17:14:50 +0800

    feat(ai): AI日报

--- gitUtils.js
+++ gitUtils.js
@@ -1,6 +1,9 @@
 import simpleGit from 'simple-git';
 const git = simpleGit('your-project-path');
-
+/*
+* git log --since="monday"  从周一开始
+* git log --since="1 month ago" 从这个月开始
+* */
 export async function getGitLogs(since = '--since="1 day ago') {
     const logs = await git.log([since]);
     return logs.all.map(entry =>`- ${entry.message}`).join('\n');
